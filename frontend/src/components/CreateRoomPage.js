d37ecc87d61e7e7510af9d2473d130d34d1e1197
iamridh1 <olaitanoladapo29@gmail.com>
2023-08-11 13:56:40 +0100

    update room create page

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -5,6 +5,7 @@ import {
     CssBaseline,
     TextField,
     FormControl,
+    FormLabel,
     FormControlLabel,
     Radio,
     RadioGroup,
@@ -14,7 +15,7 @@ import {
     Grid,
     Typography,
 } from '@mui/material'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import EarbudsIcon from '@mui/icons-material/Earbuds'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 function Copyright(props) {
@@ -39,16 +40,41 @@ export default class CreateRoomPage extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        }
+
+        this.handleVotesChange = this.handleVotesChange.bind(this)
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this)
+        this.handleCreateRoomButtonClicked = this.handleCreateRoomButtonClicked.bind(this)
     }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
+    handleVotesChange = (e) => {
+        this.setState({
+            votesToSkip: e.target.value,
         })
+    }
+
+    handleGuestCanPauseChange = (e) => {
+        this.setState({
+            guestCanPause: e.target.value === true ? true : false,
+        })
+    }
+
+    handleCreateRoomButtonClicked = (e) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            })
+        }
+
+        fetch('/api/create-room', requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
     }
 
     render() {
@@ -58,7 +84,7 @@ export default class CreateRoomPage extends Component {
                 <Grid item xs={false} sm={4} md={7}
                     sx={{
                         backgroundImage:
-                            "url(https://source.unsplash.com/random?wallpapers)",
+                            "url(https://images.unsplash.com/photo-1609081219090-a6d81d3085bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1026&q=80)",
                         backgroundRepeat: "no-repeat",
                         backgroundColor: (t) =>
                             t.palette.mode === "light"
@@ -69,46 +95,77 @@ export default class CreateRoomPage extends Component {
                     }}
                 />
                 <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <Box
-                        sx={{
-                            my: 8,
-                            mx: 4,
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, backgroundColor: "secondary.main" }}>
-                            <LockOutlinedIcon />
+                    <Box sx={{
+                        my: 8,
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}>
+                        <Avatar sx={{ m: 2, backgroundColor: "primary.main" }}>
+                            <EarbudsIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
                             Create A Room
                         </Typography>
-                        <Box component="form" noValidate onSubmit={this.handleSubmit} sx={{ mt: 1 }} align={'center'}>
-                            <TextField margin="normal" required fullWidth id="votes" label="Votes Required To Skip Song" name="votes" defaultValue={this.defaultVotes} />
-                            <RadioGroup row defaultValue={"true"}>
-                                <FormControlLabel 
-                                    value="true" 
-                                    control={<Radio color='primary' />} 
-                                    label="Play/Pause" 
-                                    labelPlacement="bottom" 
+                        
+                        <Box sx={{ mt: 1 }} align={'center'}>
+                            <Box sx={{
+                                my: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}>
+                                <FormLabel>Guest Control of Playback State</FormLabel>
+                                <RadioGroup row defaultValue={"true"} onChange={this.handleGuestCanPauseChange}>
+                                    <FormControlLabel 
+                                        value="true" 
+                                        control={<Radio color='primary' />} 
+                                        label="Play/Pause" 
+                                    />
+                                    <FormControlLabel 
+                                        value="false" 
+                                        control={<Radio color='secondary' />} 
+                                        label="No Control" 
+                                    />
+                                </RadioGroup>
+                            </Box>
+                        
+                            <Box sx={{
+                                my: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}>
+                                <TextField 
+                                    margin="normal" 
+                                    required={true}
+                                    type="number"
+                                    onChange={this.handleVotesChange}
+                                    fullWidth 
+                                    label="Votes Required To Skip Song" 
+                                    defaultValue={this.defaultVotes} 
+                                    InputProps={{
+                                        inputProps: {
+                                            min: 1, style: { textAlign: "center" }
+                                        }
+                                    }}
                                 />
-                                <FormControlLabel 
-                                    value="false" 
-                                    control={<Radio color='secondary' />} 
-                                    label="No Control" 
-                                    labelPlacement="bottom" 
-                                />
-                            </RadioGroup>
-                            <TextField margin="normal" required fullWidth id="votes" label="Votes Required To Skip Song" name="votes" defaultValue={this.defaultVotes} />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Create A Room
-                            </Button>
+                            </Box>
+
+                            <Box sx={{
+                                mb: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}>
+                                <Button type="submit" variant="contained" sx={{ mb: 2 }} onClick={this.handleCreateRoomButtonClicked}>
+                                    Create A Room
+                                </Button>
+                                <Button color="error" variant="outlined" href="/" component={Link}>
+                                    Back
+                                </Button>
+                            </Box>
                         </Box>
                     </Box>
                 </Grid>
